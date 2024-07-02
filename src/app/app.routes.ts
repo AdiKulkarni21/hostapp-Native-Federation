903821d40af34ae39288f56a649692d512f6e605
Aditya Kulkarni <adityaabk165@gmail.com>
2024-07-02 15:32:25 +0530

    hostapp-Native Federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { loadRemoteModule } from '@angular-architects/native-federation';
+export const routes: Routes = [
+    {
+        path:'ra2',
+        loadComponent : () =>
+        loadRemoteModule('mfe1','./Component').then((m)=>m.AppComponent)
+            
+        
+    },
+
+    {
+        path:'ra1',
+        loadComponent:()=>
+        loadRemoteModule('mfe3','./Component').then((m)=>m.AppComponent)
+    }
+];
